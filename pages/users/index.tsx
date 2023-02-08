e5b5311b8ad15abb950dc19b9d1a6a895ead88c0
Rushikesh Kardile <supersapiens@Orions-AirBook.local>
2023-02-08 12:42:04 +0530

    first commit

---
+++ pages/users/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "@/components/navbar/Navbar";
+import Table from "@/components/table/Table";
+function index() {
+  return (
+    <>
+      <Navbar title={"Users"} />
+      <div className="px-8 py-6">
+        <Table />
+      </div>
+    </>
+  );
+}
+
+export default index;
